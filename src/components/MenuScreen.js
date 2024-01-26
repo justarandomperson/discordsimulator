15d065757add1364abf81eba15d405c6e0fab0f2
Random <98654848+justarandomperson@users.noreply.github.com>
2024-01-27 01:13:36 +0200

    Add files via upload

--- src/components/MenuScreen.js
+++ src/components/MenuScreen.js
@@ -1,16 +1,20 @@
-import { Link } from 'react-router-dom'
-import './MenuScreen.css'
+import { Link } from "react-router-dom";
+import "./MenuScreen.css";
 
-const MenuScreen = props => {
-    return (
-        <div className="MenuScreen">
-            <h1>Text</h1>
-            <div className='MenuButtons'>
-                <Link to = "/game" className='btn'>Play</Link>
-                <Link to = "/extras" className='btn'>Extras</Link>
-            </div>
-        </div>
-    )
-}
+const MenuScreen = (props) => {
+  return (
+    <div className="MenuScreen">
+      <h1>Sans Dating Simulator</h1>
+      <div className="MenuButtons">
+        <Link to="/game" className="btn">
+          Play
+        </Link>
+        <Link to="/extras" className="btn">
+          Extras
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default MenuScreen
+export default MenuScreen;
